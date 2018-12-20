c8f7a14e873f27f74474e518b1591ae6d35bb6c5
rdvojmoc <rok.dvojmoc@gmail.com>
2018-12-20 19:16:48 +0100

    Implemented logic to handle heat pump ON/OFF logic, winter/summer logic, added comments to properties in cmAermecHeatPump and cmAermecHeatPumpModbus header files

--- Modbus.Aermec/cmAermecHeatPump.h
+++ Modbus.Aermec/cmAermecHeatPump.h
@@ -29,15 +29,38 @@ class cmAermecHeatPump {
       bool ManualOn;
       
       /**
-      ON/OFF state of the control module (heat ppump) that is copied to remote contacts, over MODBUS, etc...
+      ON/OFF state of the control module (heat pump) that is copied to remote contacts, over MODBUS, etc...
       */
       bool XS;
-      
+
+      /**
+      Winter/Summer state of the control module (heat pump) is set by software
+      */
       bool AvtoModeOn;
+      
+      /**
+      Winter/Summer state of the control module (heat pump) is set by third party (user)
+      */
       bool ManualModeOn;
+      
+      /**
+      Winter/Summer state of the control module (heat pump) that is copied to remote contacts, over MODBUS, etc...
+      */
       bool ModeXS;
+      
+      /**
+      Control module (heat pmup) alarms are detected - set by control module or the procces logic
+      */
       bool Alarm;
+
+      /**
+      Control module (heat pump) is controled by third party (user)
+      */
       bool Manual;
+
+      /**
+      Control module (heat pump) control from software or third party is not possible because preconditions for safe operation are not met
+      */
       bool Interlock;
 
       /** Process state of the properties 
@@ -46,6 +69,7 @@ class cmAermecHeatPump {
       virtual void process(int regime){
         
         XS = false;
+        ModeXS  = false;
 
         //if Alarm is present (emergency-stop button is pushed, etc...), set machine to safe state (turn of the heat pump)
         if(Alarm){
@@ -58,13 +82,25 @@ class cmAermecHeatPump {
           //if in automatic regime reset ManualOn bit(set by third party) and set XS to a current value of AvtoOn bit
           ManualOn = false;
           XS = AvtoOn;
+
+          ManualModeOn = false;
+          ModeXS = AvtoModeOn;
+          
         }
         else if (regime > REGIME_AUTO){
-          //if in manual or service regime, XS could be set by third party  
-          XS = (AvtoOn && !ManualOn || ManualOn);
+          //if in manual or service regime, XS or ModeXS could be set by third party  
+          XS = (AvtoOn && !ManualOn || ManualOn); 
+          
+          ModeXS = (AvtoModeOn && !ManualModeOn || ManualModeOn);
         }
 
-        Manual = ManualOn;
+        Manual = ManualOn || ManualModeOn;
+
+        if(Interlock && regime < REGIME_SERVICE)
+        {
+          //in case of active interlock and if machine is in automatic or manual mode set heat pump to safe state
+          XS = false;
+        }
       };
 };
 #endif
--- Modbus.Aermec/cmAermecHeatPumpModbus.h
+++ Modbus.Aermec/cmAermecHeatPumpModbus.h
@@ -9,12 +9,19 @@ class cmAermecHeatPumpModbus : public cmAermecHeatPump{
       ModbusMaster _node;
       int i = 0;
       bool state = true;
+      bool _oldXS;
+      bool _oldModeXS;
     protected:
+    
       uint8_t _slaveId;
-      uint8_t _dere; //Driver Enable/Receiver Enable
-
+      
       /**
-       * Set DE/RE state before communication is started
+      Driver Enable/Receiver Enable
+      */
+      uint8_t _dere;
+      
+      /**
+      Set DE/RE state before communication is started
       */
       void setDERE(){
         pinMode(_dere, OUTPUT);
@@ -34,6 +41,11 @@ class cmAermecHeatPumpModbus : public cmAermecHeatPump{
       };
       
     public:
+      /**
+      Code returned by Modbus library which indicates transaction success or failiure - consult Modbus library documentation to understand codes meaning
+      */
+      uint8_t TransactionResultCode;
+      
       cmAermecHeatPumpModbus(uint8_t slaveId, uint8_t dere){
         _slaveId = slaveId;
         _dere = dere;
@@ -53,40 +65,56 @@ class cmAermecHeatPumpModbus : public cmAermecHeatPump{
 
       }
 
-      /**
+      /** Override base class process method to add MODBUS functions
+      @param current regime of the process unit  0 = Automatic, 1 = Manual, 2 = Service, ...
       */
-      virtual void proccess(){
-         
-        i = i+1;
-        int result = 0;
-        // set word 0 of TX buffer to least-significant word of counter (bits 15..0)
-        _node.setTransmitBuffer(0, lowWord(i));
+      virtual void process(int regime){
+        //To optimize writing over MODBUS rembmer old state to compere it with new state
+        _oldXS = XS;
+        _oldModeXS = ModeXS;
         
-        // set word 1 of TX buffer to most-significant word of counter (bits 31..16)
-        _node.setTransmitBuffer(1, highWord(i));
-        
-        //slave: write TX buffer to (2) 16-bit registers starting at register 0
-        result = _node.writeMultipleRegisters(0, 2);
+        //Call base method before writing to ouputs
+        cmAermecHeatPump::process(regime);
 
-        // Toggle the coil at address 0x0002 (Manual Load Control)
-        result = _node.writeSingleCoil(0x0000, state);
-        state = !state;
-        
-        // slave: read (6) 16-bit registers starting at register 2 to RX buffer
-        result = _node.readHoldingRegisters(2, 6);
-        
-        // do something with data if read is successful
-        if (result == _node.ku8MBSuccess)
-        {
-          uint8_t j = 0;
-          uint16_t data[6];
-          for (j = 0; j < 6; j++)
-          {
-            data[j] = _node.getResponseBuffer(j);
-            Serial.print(data[j]);
-          }
+        //Detect state change and write it to the registers
+        if(_oldXS != XS) {
+           Serial.println("XS Changed!");
+           TransactionResultCode = _node.writeSingleCoil(0x0000, XS);
         }
-        Serial.println(result);
+
+        if(_oldModeXS != ModeXS) {
+           TransactionResultCode = _node.writeSingleCoil(0x0001, ModeXS);
+        }
+//        i = i+1;
+//        int result = 0;
+//        // set word 0 of TX buffer to least-significant word of counter (bits 15..0)
+//        _node.setTransmitBuffer(0, lowWord(i));
+//        
+//        // set word 1 of TX buffer to most-significant word of counter (bits 31..16)
+//        _node.setTransmitBuffer(1, highWord(i));
+//        
+//        //slave: write TX buffer to (2) 16-bit registers starting at register 0
+//        result = _node.writeMultipleRegisters(0, 2);
+//
+//        // Toggle the coil at address 0x0002 (Manual Load Control)
+//        result = _node.writeSingleCoil(0x0000, state);
+//        state = !state;
+//        
+//        // slave: read (6) 16-bit registers starting at register 2 to RX buffer
+//        result = _node.readHoldingRegisters(2, 6);
+//        
+//        // do something with data if read is successful
+//        if (result == _node.ku8MBSuccess)
+//        {
+//          uint8_t j = 0;
+//          uint16_t data[6];
+//          for (j = 0; j < 6; j++)
+//          {
+//            data[j] = _node.getResponseBuffer(j);
+//            Serial.print(data[j]);
+//          }
+//        }
+//        Serial.println(result);
       }
 };
 #endif
